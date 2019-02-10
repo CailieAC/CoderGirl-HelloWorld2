69ef53bd7fc4060fd8fd726b9276a1ae4bf32b08
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-09 21:09:13 -0600

    Example data types

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -9,10 +9,18 @@ namespace HelloWorld2
             Console.Write("Please enter your name: ");
 
             // Store a name the user enters to a variable.
-
+            string name = Console.ReadLine();
             // Output - Hello (name)!
-
+            Console.WriteLine($"Hello {name}!");
             Console.ReadLine();
+
+            string someString;
+            int someInt;
+            float someFloat;
+            bool someBool;
+            double someDouble = 11.2;
+            char someChar = 'a';
+
         }
     }
 }
